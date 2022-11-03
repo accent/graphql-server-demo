e4b4738803b1db906ae35e9b83a3a1d5ec5a9fb6
jankosmala <jan.kosmala@gmail.com>
2022-11-03 23:11:57 +0100

    Add book query

---
+++ BookQuery.cs
@@ -0,0 +1,25 @@
+﻿using graphql_server_db;
+using graphql_server_models;
+using Microsoft.EntityFrameworkCore;
+
+namespace graphql_server_demo;
+
+public class BookQuery
+{
+    private readonly DemoDbContext _context;
+
+    public BookQuery(DemoDbContext dbContext)
+    {
+        _context = dbContext;
+    }
+
+    public async Task<Book?> GetBook(int id)
+    {
+        return await _context.Books.FirstOrDefaultAsync(x => x.BookId == id);
+    }
+    
+    public async Task<List<Book>> GetAll()
+    {
+        return await _context.Books.ToListAsync();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using graphql_server_db;
+using graphql_server_demo;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddGraphQLServer();
+builder.Services.AddGraphQLServer()
+    .AddQueryType<AuthorQuery>()
+    .AddQueryType<BookQuery>();
 builder.Services.AddDbContext<DemoDbContext>(options => options.UseSqlServer());
 var app = builder.Build();
 
