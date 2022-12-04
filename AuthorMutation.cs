6fc2f220b7879260bd7eb972d3b375cfb1f5c5f1
jankosmala <jan.kosmala@gmail.com>
2022-12-04 13:36:20 +0100

    add mutations

---
+++ AuthorMutation.cs
@@ -0,0 +1,21 @@
+﻿using graphql_server_db;
+using Microsoft.EntityFrameworkCore;
+
+namespace graphql_server_demo;
+
+[ExtendObjectType(Name="Mutation")]
+public class AuthorMutation
+{
+    public async Task<bool> RemoveAuthor([Service(ServiceKind.Resolver)]DemoDbContext dbContext, [ID] int id)
+    {
+        var author = await dbContext.Authors.FirstOrDefaultAsync(x => x.AuthorId == id);
+        if (author != null)
+        {
+            dbContext.Authors.Remove(author);
+            await dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        return false;
+    }
+}
